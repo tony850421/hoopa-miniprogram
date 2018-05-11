7213a8f1622105178a6c7e6e78d68dd257d2355b
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-05-11 15:00:56 +0800

    modifing product page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,16 @@
 //index.js
 
 const AV = require('../../utils/av-weapp-min');
+const AVLive = require('../../utils/av-live-query-weapp-min');
+const bind = require('../../utils/live-query-binding');
 
 const appInstance = getApp()
 
 Page({
   data: {
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    products: []
   },
   showInput: function () {
     this.setData({
@@ -30,7 +33,11 @@ Page({
       inputVal: e.detail.value
     });
   },
-  goToProject: function(){
+  goToProject: function (e) {    
+    wx.setStorage({
+      key: "projectID",
+      data: e.currentTarget.id
+    })
     wx.navigateTo({
       url: '../project/project',
     })
@@ -48,7 +55,6 @@ Page({
   onReady: function () {
     wx.getUserInfo({
       success: res => {
-        // 可以将 res 发送给后台解码出 unionId
         var user = AV.User.current();
         if (user) {
           user.set('nickName', res.userInfo.nickName);
@@ -72,30 +78,59 @@ Page({
                 relation.add(user);
                 return role.save();
               }).then(function (role) {
-                console.log("role asigned ok");
               }).catch(function (error) {
                 console.log(error);
               });
             }
           }).then(function (administratorRole) {
-            //此时 administratorRole 已经包含了当前用户
+
           }).catch(function (error) {
-            // 输出错误
             console.log(error);
           });
         }
-
-        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-        // 所以此处加入 callback 以防止这种情况
         if (this.userInfoReadyCallback) {
           this.userInfoReadyCallback(res)
         }
       }
     })
   },
-  goToPartners: function(){
+  goToPartners: function () {
     wx.navigateTo({
       url: '../partners/partners',
     })
+  },
+  fetchProducts: function (user) {
+    console.log('uid', user.id);
+    const query = new AVLive.Query('Project');
+    query.include('creator');
+    query.include('image');
+    query.descending('createdAt');
+    query.limit(5);
+    const setProducts = this.setProducts.bind(this);
+    return AVLive.Promise.all([query.find().then(setProducts), query.subscribe()]).then(([products, subscription]) => {
+      this.subscription = subscription;
+      if (this.unbind) this.unbind();
+      this.unbind = bind(subscription, products, setProducts);
+    }).catch(error => console.error(error.message));
+  },
+  onReady: function () {
+    const user = AV.User.current();
+    this.fetchProducts(user);
+  },
+  onUnload: function () {
+    this.subscription.unsubscribe();
+    this.unbind();
+  },
+  onPullDownRefresh: function () {
+    const user = AV.User.current();
+    if (!user) return wx.stopPullDownRefresh();
+    this.fetchProducts(user).catch(error => console.error(error.message)).then(wx.stopPullDownRefresh);
+  },
+  setProducts: function (products) {
+    console.log(products);
+    this.setData({
+      products,
+    });
+    return products;
   }
 });
